e86c9c4f68ffbca8ebed8a34ffdef21fab35727a
Federico Cassina Carbia <83524719+fedecassina@users.noreply.github.com>
2023-11-29 20:55:54 -0300

    toto for the win

--- src/main/java/domain/controllers/Controller.java
+++ src/main/java/domain/controllers/Controller.java
@@ -1,16 +1,20 @@
 package domain.controllers;
 
 import domain.models.entities.usuario.Persona;
+import domain.server.Server;
 import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import io.javalin.http.Context;
 
+import javax.persistence.EntityManager;
+
 
 public abstract class Controller implements WithSimplePersistenceUnit {
-
+    EntityManager entityManager = Server.createEntityManager();
+    
     protected Persona usuarioLogueado(Context ctx) {
         if(ctx.sessionAttribute("usuario_id") == null)
             return null;
-        return entityManager()
+        return entityManager
                 .find(Persona.class, ctx.sessionAttribute("usuario_id"));
     }
 }
