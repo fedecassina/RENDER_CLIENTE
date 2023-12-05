e2340a4f2397bde76702316fbf54e617ab6beddf
tomasmartinez02 <102559756+tomasmartinez02@users.noreply.github.com>
2023-12-05 14:46:09 -0300

    prueba pusheo

--- src/main/java/domain/models/entities/clienteAPI/ApiResponse.java
+++ src/main/java/domain/models/entities/clienteAPI/ApiResponse.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class ApiResponse<T> {
+public class ApiResponse<T> { //a ver si me deja pushear
     private int codigoDeEstado;
     private String error;
     private boolean exito;
